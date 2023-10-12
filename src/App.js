6bb6c447f199af0f0dfa9594e34649a8328fbca6
Williams E. Fernández <williamsenrique@gmail.com>
2023-10-12 16:07:53 -0600

    Realizando configuración de las rutas para el aplicativo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
 
 function App() {
   return (
     <div className="container">
-      <Navegacion/>
-      <ListadoEmpleados/>
+      <BrowserRouter>
+        <Navegacion />
+        <Routes>
+          <Route exact path='/' element={<ListadoEmpleados />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
